736afda878b1f2db8ca31144bee6d6c45e604e38
mollinaca <github@mail.watarinohibi.tokyo>
2020-03-22 22:28:13 +0900

    AC

--- code/abc/159/b.py
+++ code/abc/159/b.py
@@ -9,17 +9,14 @@ def j(x:str):
             return False
     return True
 
-#print (s)
 if not j(s):
     print ("No")
     exit ()
 
-#print (s[0:((n-1)//2)])
 if not j(s[0:((n-1)//2)]):
     print ("No")
     exit ()
 
-#print (s[((n+3)//2)-1::])
 if not j(s[((n+3)//2)-1::]):
     print ("No")
     exit ()
