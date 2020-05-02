edff8d3bcf97517bdcec4c796f6fdcd4f8d89c2c
mollinaca <github@mail.watarinohibi.tokyo>
2020-05-02 23:01:51 +0900

    AC

--- code/abc/165/d.py
+++ code/abc/165/d.py
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 from math import floor
 a,b,n = map(int,input().split())
-x = ((b//n)*n)-1
-print ( max((floor((a*x/b) - a*(floor(x/b)))),(floor((a*n/b) - a*(floor(n/b))))) )
+x = min(b-1,n)
+print (floor((a*x/b) - a*(floor(x/b))))
