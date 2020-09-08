8b00fdf6c2994779de9769229c9e1445f60a986a
mollinaca <dev@m.rel0.pw>
2020-09-08 21:32:09 +0900

    AC

---
+++ code/practice/arc025/a.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+d = list(map(int,input().split()))
+j = list(map(int,input().split()))
+
+ans = 0
+for a,b in zip(d,j):
+    ans += max(a,b)
+
+print (ans)
